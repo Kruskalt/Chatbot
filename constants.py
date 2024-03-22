6710795fa2bec64d7654af0dc7a2a3500ad61b87
nissero <matiasnissero@gmail.com>
2024-03-22 15:26:01 -0300

    el bot fue modificado para saludar en español

--- constants.py
+++ constants.py
@@ -1,7 +1,43 @@
 SWEETS = ('🍬', '🍭', '🍫', '🍰', '🍪', '🍩', '🍨', '🍮', '☕')
-GREETING_INPUTS = ('hello', 'hi', 'greetings', "what's up?", 'sup', 'hey', 'yo', "how's it going?", 'how are you doing?', "what's going on?", 'good to see you', 
-	'nice to see you', 'good morning', 'good afternoon', 'good_evening', "it's nice to meet you", 'pleased to meet you')
-GREETING_REPLIES = ('Hi!', 'Hey!', '*nods*', 'Hi there!', 'Hello!', "I'm glad you are talking to me!")
-THANKS_INPUTS = ('thanks', 'thank you', 'thanks a lot', 'I owe you one', 'cheers', 'ta', 'I really appreciate it', "that's really nice of you", 'thanks once again', 
-	'thank you for your consideration')
-THANKS_REPLIES = ("You're welcome!", 'You got it', 'No worries!', 'Not a problem', 'My pleasure!', 'It was nothing!', "I'm happy to help", 'Not at all!', 'Anytime!')
+
+GREETING_INPUTS = ('hola', 'buenas', "que onda?", 'como estas?', 'hey', 'que haces?', "todo bien?", 'que bueno verte', "me alegro de verte", 'good to see you', 
+					'buenos dias', 'buenas tardes', 'buenas noches', "me alegro de conocerte", 'me alegro de haberte encontrado')
+GREETING_REPLIES = ('Hola!', 'Hey!', '*nods*', 'Hi there!', 'Hello!', "Buenas", "Que bueno verte!")
+
+THANKS_INPUTS = ('gracias', 'muchas gracias', 'te debo una', 'lo aprecio mucho', "que bueno de tu parte", 'gracias otra vez', 
+	'muchisimas gracias', 'sos el mejor', 'gracias mauricio')
+THANKS_REPLIES = ("¡De nada!", "¡Entendido!", "¡No hay problema!", "¡Un placer!", "No fue nada!", "Me alegra poder ayudar", "¡Para nada!", "¡En cualquier momento!")
+
+STOP_WORDS_SPANISH = ["de", "la", "que", "el", "en", "y", "a", "los", "del", "se", "las", "por", 
+    "un", "para", "con", "no", "una", "su", "al", "lo", "como", "más", "pero", 
+    "sus", "le", "ya", "o", "este", "sí", "porque", "esta", "entre", "cuando", 
+    "muy", "sin", "sobre", "también", "me", "hasta", "hay", "donde", "quien", 
+    "desde", "todo", "nos", "durante", "todos", "uno", "les", "ni", "contra", 
+    "otros", "ese", "eso", "ante", "ellos", "e", "esto", "mí", "antes", "algunos", 
+    "qué", "unos", "yo", "otro", "otras", "otra", "él", "tanto", "esa", "estos", 
+    "mucho", "quienes", "nada", "muchos", "cual", "poco", "ella", "estar", "estas", 
+    "algunas", "algo", "nosotros", "mi", "mis", "tú", "te", "ti", "tu", "tus", "ellas", 
+    "nosotras", "vosostros", "vosostras", "os", "mío", "mía", "míos", "mías", "tuyo", "tuya", 
+    "tuyos", "tuyas", "suyo", "suya", "suyos", "suyas", "nuestro", "nuestra", "nuestros", 
+    "nuestras", "vuestro", "vuestra", "vuestros", "vuestras", "esos", "esas", "estoy", "estás", 
+    "está", "estamos", "estáis", "están", "esté", "estés", "estemos", "estéis", "estén", "estaré", 
+    "estarás", "estará", "estaremos", "estaréis", "estarán", "estaría", "estarías", "estaríamos", 
+    "estaríais", "estarían", "estaba", "estabas", "estábamos", "estabais", "estaban", "estuve", 
+    "estuviste", "estuvo", "estuvimos", "estuvisteis", "estuvieron", "estuviera", "estuvieras", 
+    "estuviéramos", "estuvierais", "estuvieran", "estuviese", "estuvieses", "estuviésemos", 
+    "estuvieseis", "estuviesen", "estando", "estado", "estada", "estados", "estadas", "estad", 
+    "he", "has", "ha", "hemos", "habéis", "han", "haya", "hayas", "hayamos", "hayáis", "hayan", 
+    "habré", "habrás", "habrá", "habremos", "habréis", "habrán", "habría", "habrías", "habríamos", 
+    "habríais", "habrían", "había", "habías", "habíamos", "habíais", "habían", "hube", "hubiste", 
+    "hubo", "hubimos", "hubisteis", "hubieron", "hubiera", "hubieras", "hubiéramos", "hubierais", 
+    "hubieran", "hubiese", "hubieses", "hubiésemos", "hubieseis", "hubiesen", "habiendo", "habido", 
+    "habida", "habidos", "habidas", "soy", "eres", "es", "somos", "sois", "son", "sea", "seas", "seamos", 
+    "seáis", "sean", "seré", "serás", "será", "seremos", "seréis", "serán", "sería", "serías", "seríamos", 
+    "seríais", "serían", "era", "eras", "éramos", "erais", "eran", "fui", "fuiste", "fue", "fuimos", "fuisteis", 
+    "fueron", "fuera", "fueras", "fuéramos", "fuerais", "fueran", "fuese", "fueses", "fuésemos", "fueseis", 
+    "fuesen", "sintiendo", "sentido", "sentida", "sentidos", "sentidas", "siente", "sentid", "tengo", "tienes", 
+    "tiene", "tenemos", "tenéis", "tienen", "tenga", "tengas", "tengamos", "tengáis", "tengan", "tendré", "tendrás", 
+    "tendrá", "tendremos", "tendréis", "tendrán", "tendría", "tendrías", "tendríamos", "tendríais", "tendrían", 
+    "tenía", "tenías", "teníamos", "teníais", "tenían", "tuve", "tuviste", "tuvo", "tuvimos", "tuvisteis", "tuvieron", 
+    "tuviera", "tuvieras", "tuviéramos", "tuvierais", "tuvieran", "tuviese", "tuvieses", "tuviésemos", "tuvieseis", "tuviesen", 
+    "teniendo", "tenido", "tenida", "tenidos", "tenidas", "tened"]
