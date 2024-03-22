d72069618718ea0b62081e5d7d302d6dfdaed17c
Kruskalt <codigorap13@gmail.com>
2024-03-22 12:55:08 -0300

    dejo algunos comentarios, lo de tfidf esta complicado pero hay una linea resumida.

--- bot-desktop.py
+++ bot-desktop.py
@@ -5,8 +5,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import string
 import warnings
-
-def get_formalities_response(formality) : #se fija si esta in greet  y lo hace para cada greet 
+                                            
+def get_formalities_response(formality) : #verifica si la formalidad del saludo esta en la lista
     if any(remove_punctuation_marks(formality).lower() in remove_punctuation_marks(greet).lower() for greet in constants.GREETING_INPUTS) :
         return random.choice(constants.GREETING_REPLIES)
     elif any(remove_punctuation_marks(formality).lower() in remove_punctuation_marks(thanks).lower() for thanks in constants.THANKS_INPUTS) :
@@ -17,11 +17,13 @@ def get_lemmatized_tokens(text) :
     return [nltk.stem.WordNetLemmatizer().lemmatize(normalized_token) for normalized_token in normalized_tokens]
     #lematiza los tokens,pasa a su forma raiz es decir de donde deriva la palabra
     #estudiosos => estudioso    , aman => amar , felices => feliz
-def get_query_reply(query) :
-    documents.append(query)
+    #se normaliza y simplifica el texto para su analisis
+    
+    
+def get_query_reply(query) :  #se usa un algoritmo matematico, para analizar la frecuencia de una palabra y en base a eso elige la respuesta. 
+    documents.append(query)                                             #palabras vacias se sacan (a,is,the,are)
     tfidf_results = TfidfVectorizer(tokenizer = get_lemmatized_tokens, stop_words = 'english').fit_transform(documents)
     cosine_similarity_results = cosine_similarity(tfidf_results[-1], tfidf_results).flatten()
-    # The last will be 1.0 because it is the Cosine Similarity between the first document and itself
     best_index = cosine_similarity_results.argsort()[-2]
     documents.remove(query)
     if cosine_similarity_results[best_index] == 0 :
@@ -29,9 +31,9 @@ def get_query_reply(query) :
     else :
         return documents[best_index]
 
-def remove_punctuation_marks(text) :  #eliminar caracteres
-    punctuation_marks = dict((ord(punctuation_mark), None) for punctuation_mark in string.punctuation) #crea un dicc
-    return text.translate(punctuation_marks)
+def remove_punctuation_marks(text) :  #eliminar caracteres , crea un dicc tipo tupla con el valor unicode del caracter
+    punctuation_marks = dict((ord(punctuation_mark), None) for punctuation_mark in string.punctuation) 
+    return text.translate(punctuation_marks) #elimina los caracteres con el mapeo previamente hecho
 
 if __name__ == "__main__" :
     warnings.filterwarnings("ignore")
