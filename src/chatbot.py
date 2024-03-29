b5fee2e19d069aac10174b9569e603221f4cd28a
Kruskalt <codigorap13@gmail.com>
2024-03-29 12:17:13 -0300

    separe el codigo que esta en training en funciones y corri de lugar unos archivos necesarios.

--- src/chatbot.py
+++ src/chatbot.py
@@ -11,7 +11,7 @@ from keras.models import load_model
 lemmatizer = WordNetLemmatizer()
 
 #Importamos los archivos generados en el código anterior
-intents = json.loads(open('intents.json').read())
+intents = json.loads(open('../intents.json').read())
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
 model = load_model('my_model.keras')
